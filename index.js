60f4a08073878adaea8c57e9ba5ed81580c8b845
David Ohayon <davideytanohayon@gmail.com>
2021-02-04 13:40:55 +0200

    Update prototype for lessons message

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 require("dotenv").config();
+require("lessons");
 const Discord = require("discord.js");
 
 const client = new Discord.Client();
 
+const prefix = "!";
+
+// get which current lesson there is
+const date = new Date();
+
+sunday = date.getDay() == 0;
+monday = date.getDay() == 1;
+tuesday = date.getDay() == 2;
+wednesday = date.getDay() == 3;
+thursday = date.getDay() == 4;
+
+const lesson = () => {
+  lessons = {};
+};
+
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}! caca`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("message", (msg) => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!");
-    msg.send(msg);
+  if (msg.channel.id === "805852579161833572") {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (message.content.startsWith(`${prefix}lesson`)) {
+      message.channel.send(lesson());
+    }
   }
 });
 
---
+++ lessons.js
@@ -0,0 +1,31 @@
+// lesson links
+const hebrew =
+  "https://us04web.zoom.us/j/77765386769?pwd=UE51NmNjL2Y1elNlMW0rcXJyUkNWZz09";
+
+const gemara = "https://edu-il.zoom.us/j/2960827055";
+
+const math =
+  "https://edu-il.zoom.us/j/3198379053?pwd=elcrU1Nxb3hpaGNOQzVBNzhiOHR4Zz09";
+
+const english_nakar = "https://edu-il.zoom.us/j/6259151077";
+
+const english_nourit =
+  "https://edu-il.zoom.us/j/4986899586?pwd=bTlKd00yc1Iyd20wUWNoREF4bG5lQT09";
+
+const programming =
+  "https://edu-il.zoom.us/j/7184928368?pwd=cmZ5Sm9BdXdtWEcrcFBWN0pRRlZjUT09";
+
+const history_itsik = "https://edu-il.zoom.us/j/3136023920";
+
+const literature =
+  "https://edu-il.zoom.us/j/9655244107?pwd=c1h5RGNyYUs5QndDUHpqTFMyUmFqUT09";
+
+const torah_dov = "https://edu-il.zoom.us/j/3935560602";
+
+const history_tsipi = "https://edu-il.zoom.us/j/5238947742";
+
+const torah_rabinak =
+  "https://edu-il.zoom.us/j/3422464676?pwd=aUlOWFFjUEx0ak5yalFpTjBHdU1Xdz09";
+
+const physics =
+  "https://zoom.us/j/98729071010?pwd=ajVBbU13bHBqdmhCemlqQWV2aEZFUT09";
