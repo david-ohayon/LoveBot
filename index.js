e6f7f939d764ebcfcec0ae59d5b9076e232f234b
David Ohayon <davideytanohayon@gmail.com>
2021-02-04 23:18:32 +0200

    im such a dumbass

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("msg", (msg) => {
+client.on("message", (msg) => {
   // if (msg.content === "ping") {
   //   msg.reply("Pong!");
   // }
